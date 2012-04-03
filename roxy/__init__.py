8f3188c2343fbc10222fbb6f3ec6510354da9e68
Euam Chantarasombat <euam@proteus-tech.com>
2012-04-03 15:41:48 +0000

    forward proper headers and content

--- roxy/__init__.py
+++ roxy/__init__.py
@@ -1,3 +1,11 @@
 """
 Reverse proxy app
 """
+from django.http import HttpRequest
+
+
+HttpRequest._orig_load_post_and_files = HttpRequest._load_post_and_files
+def _load_post_and_files(self):
+    self.raw_post_data
+    return self._orig_load_post_and_files()
+HttpRequest._load_post_and_files = _load_post_and_files
